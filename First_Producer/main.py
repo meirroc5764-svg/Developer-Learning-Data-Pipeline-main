b92591fb7a9495c6ac7f6211d4a0d9b2ac56d961
Meir <meirroc5764@gmail.com>
2026-09-04 02:44:01 +0300

    finish script2 and first producer

--- First_Producer/main.py
+++ First_Producer/main.py
@@ -1,6 +1,7 @@
 from confluent_kafka import Producer
 from pathlib import Path
-import pandas as pd
+import json
+import csv
 
 producer = Producer({
     "bootstrap.servers":"localhost:9092"
@@ -9,18 +10,20 @@ producer = Producer({
 path = list(Path("C:/Users/Aenigma/OneDrive/Desktop/Developer-Learning-Data-Pipeline-main").rglob("developer_ai_learning_raw.csv"))
 print(path[0])
 
-df = pd.read_csv(path[0])
+if not path:
+    raise FileNotFoundError("Not Found")
 
+with open(path[0],"r", encoding="utf-8") as f:
+    all_data = csv.DictReader(f)
 
-df = df.drop_duplicates()
+    for data in all_data:
 
-for _, item in df.iterrows():
+        message = json.dumps(data, ensure_ascii=False)
 
-    message = item.to_json()
-    # print(message)
-
-    producer.produce(
-        "notvalidData",
-        value=message
-    )
+        producer.produce(
+            "notvalidData",
+            value=message
+        )
+        
 producer.flush()
+print("finish send to kafka")
